f3fe9a08e77d78a03cc8559fb13434533d741fce
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-14 01:49:58 -0700

    Create printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to the program that print string using prints function.
+ *
+ * Return: integer value was return
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
