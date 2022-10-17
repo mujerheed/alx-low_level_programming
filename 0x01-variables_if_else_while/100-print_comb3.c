d57cdba05b15a0e35c71634a79918985f156b7c7
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-17 15:43:35 +0000

    Create print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Return integer value
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{	
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if ((i != 8) || (j != 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
