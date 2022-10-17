b940506e37236a1cd3df5ced76f051bf953589f8
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-17 09:42:35 +0000

    Update print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main- Entry point
+ *Return: End point
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if ((c == 'e') || (c == 'q'))
+			continue;
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
