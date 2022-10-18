dd36219625bc9dea913462efc101a1696148f063
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-18 20:00:14 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,16 @@
 #ifndef _MAIN_HEADER_H_
 #define _MAIN_HEADER_H_
 
-/* _puchar - function general*/
+/* _puchar - function general */
 int _putchar(char);
 
-/* print_alphabet - funtion 1*/
+/* print_alphabet - function 1 */
 void print_alphabet(void);
+
+/* print_aphabet_x10 - function 2 */
+void print_alphabet_x10(void);
+
+/* _islower - function 3 */
+int _islower(int);
 
 #endif
