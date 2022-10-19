927bfefb7b2ba630576dd9e1bd6d2f5f5f4719ab
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-19 13:51:54 +0000

    Update times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int a, b, mul;
+
+	a = 0;
+	while (a <= 9)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			mul = a * b;
+			_putchar(48 + mul);
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		a++;
+	}
+}
