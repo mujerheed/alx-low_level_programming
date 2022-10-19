8b3927769c5ca84174e77106ca446c5d4e22f861
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-19 01:58:25 -0700

    Create print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98.
+ * @n: collect integer value
+ */
+void print_to_98(int n)
+{
+	for (; n >= 98; n--)
+	{
+		_putchar(n + '0');
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	while (n <= 98)
+	{
+		_putchar(n + '0');
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++;
+	}
+}
