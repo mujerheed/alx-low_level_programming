1780cc6ddd9a61bbc3a648754550d89e6d3d7e45
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-20 00:20:07 +0000

    Update fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,20 +6,30 @@
  */
 int main(void)
 {
-	unsigned long int a = 1, b = 2, next;
-	int i;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	printf("%lu, %lu, ", a, b);
-	for (i = 1; i < 98; i++)
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		next = a + b;
-		printf("%lu", next);
-		a = b;
-		b = next;
-		if (i != 97)
+		if (bk1 + bk2 > 10000000000 || fr2 > 0 || fr1 > 0)
 		{
-			printf(", ");
+			hold1 = (bk1 + bk2) / 10000000000;
+			hold2 = (bk1 + bk2) % 10000000000;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
 		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
 	putchar('\n');
 	return (0);
