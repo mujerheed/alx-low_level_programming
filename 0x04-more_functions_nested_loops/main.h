32c7e744d08dc8eddef5397f79123701946f2122
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-22 22:33:01 +0000

    Update main header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef _MAIN_HEADER_H_
+#define _MAIN_HEADER_H_
+
+/* _puchar - function general */
+int _putchar(char);
+
+/* _isupper - function 1 */
+int _isupper(int c);
+
+/* _isdigit - function 2 */
+int _isdigit(int c);
+
+/* print_numbers - function 3 */
+void print_numbers(void);
+
+/* print_most_numbers - function 4 */
+void print_most_numbers(void);
+
+/* more_numbers - function 5 */
+void more_numbers(void);
+
+/* print_liine - function 6 */
+void print_line(int n);
+
+#endif
