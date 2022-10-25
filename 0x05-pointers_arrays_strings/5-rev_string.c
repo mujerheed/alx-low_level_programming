1634d0310645991f2f60a7c5c0e8bdd4429f5698
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-25 12:38:24 -0700

    Update Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <string.h>
+
 /**
   * rev_string - function tgat reverse a string
   * @s: string pointer
@@ -7,18 +9,9 @@ void rev_string(char *s)
 {
 	int m;
 
-	for (m = 8; m >= 0; m--)
+	for (m = strlen(s) - 1; m >= 0; m--)
 	{
 		_putchar(s[m]);
 	}
 	_putchar('\n');
-}
-int main(void)
-{
-	    char s[10] = "My School";
-
-	        printf("%s\n", s);
-		    rev_string(s);
-		        printf("%s\n", s);
-			    return (0);
 }
