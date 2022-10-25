1a8c57d4bdf5cfe4b2a9a5b58dc2b5bb340a1ee0
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-25 11:11:24 -0700

    Update Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -14,10 +14,3 @@ void _puts(char *str)
 	}
 	_putchar('\n');
 }
-int main(){
-	char *str;
-
-	    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	        _puts(str);
-		    return (0);
-}
