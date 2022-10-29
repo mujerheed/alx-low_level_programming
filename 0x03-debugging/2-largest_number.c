d94060eeffbac239d2938952fd57c8783a061c9e
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-29 15:13:27 -0700

    Update largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,29 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > ac)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else if(c > a && c > b)
+	else
 	{
 		largest = c;
 	}
 
 	return (largest);
+}
+int main(void)
+{
+        int a, b, c;
+        int largest;
+
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
 }
