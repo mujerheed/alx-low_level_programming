8526475b57e1ade2f671c94e1b230c7f63f74201
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-10-29 23:43:02 +0000

    Update more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,7 @@ void more_numbers(void)
 		{
 			if (q > 9)
 			{
-				_putchar(q / 10 + '0');
+				_putchar('1');
 				_putchar(q % 10 + '0');
 			}
 			else
@@ -25,4 +25,9 @@ void more_numbers(void)
 		_putchar('\n');
 		p++;
 	}
+}
+int main(void)
+{
+    more_numbers();
+    return (0);
 }
