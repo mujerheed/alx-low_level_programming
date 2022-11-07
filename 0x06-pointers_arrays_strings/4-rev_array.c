4394b0c87947bcbcc7989504c2948259f14e0d31
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-11-07 09:44:45 +0000

    Update Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -17,35 +17,3 @@ void reverse_array(int *a, int n)
 		a[j] = t[j];
 	a[j] = '\0';
 }
-void print_array(int *a, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (i != 0)
-			printf(", ");
-		printf("%d", a[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int a[] = {
-		234234, 64534, 77632, 8723451, 98987, 1345231, 98534597,
-		23456789, 6235456, 41235416, 374578, 10435434, 89734567
-	};
-
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
-	return (0);
-}
