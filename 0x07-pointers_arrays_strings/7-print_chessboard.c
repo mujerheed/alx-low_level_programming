fd4f2f0d68c851749b7fe933422339a15e7fd57e
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-11-08 22:00:03 +0000

    Update Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: character array
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int d = 0, f;
+
+	while(d < 8)
+	{
+		for (f = 0; f < 8; f++)
+		{
+			_putchar(a[d][f]);
+		}
+		_putchar('\n');
+		d++;
+	}
+}
