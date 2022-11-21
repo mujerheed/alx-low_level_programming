3eb78f0e580010d505d3473fdcaea2ab125ffdb6
Abdurrazaq Jibril <mujerheed20@gmail.com>
2022-11-21 22:16:04 +0000

    Update Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] >= '\0')
 	{
 		if (*(s + i) == c)
 			return (s + i);
 		i++;
 	}
-	if (c == '\0')
-		return (s);
 
 	return (NULL);
 }
